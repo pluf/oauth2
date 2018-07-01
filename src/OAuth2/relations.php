58cf93a7b007428068b60f93ae475e0bf0f0795c
mostafabarmshory <mostafa.barmshory@dpq.co.ir>
2018-07-01 17:59:24 +0430

    Project scafolding

---
+++ src/OAuth2/relations.php
@@ -0,0 +1,17 @@
+<?php
+
+return array (
+// 		'Jayab_Vote' => array (
+// 				'relate_to' => array (
+// 						'User',
+// 						'Jayab_Location' 
+// 				) 
+// 		),
+// 		'Jayab_Location' => array (
+// 				'relate_to_many' => array (
+// 						'KM_Label',
+// 						'KM_Category',
+// 				        'Jayab_Tag'
+// 				) 
+// 		) 
+);
